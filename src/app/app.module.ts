93833b60f3204e2fed940f14a0911b596530a8e6
Kateryna Levytska <klevytska@gk-software.com>
2018-06-05 18:35:22 +0200

    Added resume components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterModule} from '@angular/router';
+import {ContactsComponent} from './components/contacts/contacts.component';
+import {HobbiesComponent} from './components/hobbies/hobbies.component';
+import {EducationComponent} from './components/education/education.component';
+import {ExperienceComponent} from './components/experience/experience.component';
+import {BlogComponent} from './components/blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ExperienceComponent,
+    EducationComponent,
+    HobbiesComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'contacts', component: ContactsComponent},
+      {path: 'experience', component: ExperienceComponent},
+      {path: 'education', component: EducationComponent},
+      {path: 'hobbies', component: HobbiesComponent},
+      {path: 'blog', component: BlogComponent}
+    ])
+  ],
+  exports: [
+    MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+
 })
 export class AppModule { }
